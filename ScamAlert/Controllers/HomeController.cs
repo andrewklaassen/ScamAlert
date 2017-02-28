f6755b85b840f8a0b70e22c5ff8885e64b8e4a3b
Andrew <klaassenandrew@gmail.com>
2017-02-28 13:59:56 -0600

    Added ScamReport view for a ScamId

--- ScamAlert/Controllers/HomeController.cs
+++ ScamAlert/Controllers/HomeController.cs
@@ -1,8 +1,9 @@
 ﻿using ScamAlert.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ScamAlert.Controllers
@@ -16,7 +17,49 @@ namespace ScamAlert.Controllers
             var sCAM = db.Scams;
             return View(sCAM.ToList());
         }
-        //TODO: Need to build Scam Report method
+        public ActionResult View(int scamId)
+        {
+            try
+            {
+                var model = new ScamReports
+                {
+                    ScamReportss = getScamReportss(scamId),
+                    ScamId = scamId   
+                };
+                List<ScamReports> modelList = new List<ScamReports>();
+                modelList.Add(model);
+                //Return the view
+                return View(modelList);
+            }
+            catch (Exception e)
+            {
+                return RedirectToAction("IndexError", "Home");
+            }
+        }
 
+        private List<ScamReport> getScamReportss(int scamId)
+        {
+            // Open a Sql Connection
+            SqlConnection sqlConnection1 = new SqlConnection("data source=cs.cofo.edu;initial catalog=aklaassen;persist security info=True;user id=aklaas01;password=paint123;");
+            SqlCommand cmd = new SqlCommand("uspGetScamReports", sqlConnection1);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@ScamId", SqlDbType.Int).Value = scamId;
+            sqlConnection1.Open();
+            // Create a reader to get a list
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<ScamReport> scamrList = new List<ScamReport>();
+            // Create a list using the reader
+            while (reader.Read())
+            {
+                ScamReport scamreport = new ScamReport();
+                scamreport.report = reader["report"].ToString();
+                scamreport.timePosted = DateTime.Parse(reader["timePosted"].ToString());
+                scamreport.User = new User();
+                scamreport.User.userName = reader["userName"].ToString();
+                scamrList.Add(scamreport);
+            }
+            sqlConnection1.Close();
+            return scamrList;
+        }
     }
 }
---
+++ ScamAlert/Models/ScamReports.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScamAlert.Models
+{
+    public class ScamReports
+    {
+        public List<ScamReport> ScamReportss { get; set; }
+        public int ScamId { get; set; }
+    }
+}
